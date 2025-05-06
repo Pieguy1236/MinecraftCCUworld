89ba9d14bbecc5140432e26a507b292c92f18670
pieguy1236 <biggerthanyou1lmao@gmail.com>
2025-05-06 15:50:56 -0400

    test

--- README.md
+++
@@ -1,4 +0,0 @@
-# MinecraftCCUworld
-minecraft world for friendslol
-
-I hope your ready to learn git
---
+++ repeatable_commits.bat
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
+git fetch https://github.com/Pieguy1236/MinecraftCCUworld test
+git branch -m test
+git checkout test
+
+pause
