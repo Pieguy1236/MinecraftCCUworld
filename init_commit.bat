b6ecc31c65f43f4a19cd92933a54739dc224ee0e
pieguy1236 <biggerthanyou1lmao@gmail.com>
2025-05-06 15:46:37 -0400

    initial commit

--- init_commit.bat
+++ init_commit.bat
@@ -1,7 +1,7 @@
 git init
 git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
 git add .
-git branch -m main
+git branch -m test
 git commit -m "initial commit"
-git push origin main
+git push origin test
 pause
