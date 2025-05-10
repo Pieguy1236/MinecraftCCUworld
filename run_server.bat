1c5e15f9ed78dffff6605e895bd85073a00539b2
pieguy1236 <biggerthanyou1lmao@gmail.com>
2025-05-10 03:26:11 -0400

    updating data

--- run_server.bat
+++ run_server.bat
@@ -17,7 +17,7 @@ git init
 git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
 git add .
 git branch -m JC_branch
-git commit -m "update wrold"
+git commit -m "update world"
 git push origin -f JC_branch
 
 echo "terminal can be closed"
