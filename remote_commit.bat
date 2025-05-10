3e2cf9f5b4be9216604c982d00ca91bd8c42754a
pieguy1236 <biggerthanyou1lmao@gmail.com>
2025-05-10 03:24:53 -0400

    updating data

--- remote_commit.bat
+++ remote_commit.bat
@@ -2,6 +2,6 @@ git init
 git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
 git add .
 git branch -m JC_branch
-git commit -m "initial commit"
+git commit -m "updating data"
 git push origin -f JC_branch
 pause
