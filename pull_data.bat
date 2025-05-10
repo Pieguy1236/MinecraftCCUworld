f01e518d59c2458460b0270e341ea906a8313da6
pieguy1236 <biggerthanyou1lmao@gmail.com>
2025-05-10 03:18:36 -0400

    update wrold

---
+++ pull_data.bat
@@ -0,0 +1,9 @@
+git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
+git branch -m test
+git fetch
+git status
+git reset origin/test
+git restore .
+
+
+pause
---
+++ run_server.bat
@@ -0,0 +1,26 @@
+echo "downloading server data"
+
+git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
+git branch -m test
+git fetch
+git status
+git reset origin/test
+git restore .
+
+echo "starting server"
+
+java -Xmx12288M -Xms12288M -jar forge.jar nogui
+
+echo "uploading server data"
+
+git init
+git remote add origin https://github.com/Pieguy1236/MinecraftCCUworld
+git add .
+git branch -m JC_branch
+git commit -m "update wrold"
+git push origin -f JC_branch
+
+echo "terminal can be closed"
+
+pause
+
